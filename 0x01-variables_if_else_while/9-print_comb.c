50c507fef5a73249743abb25abfe4e15f746284e
mokhatrmramadan <mokhtarramdanformal@gmail.com>
2023-06-16 11:24:03 -0700

    Fix on print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,10 +10,18 @@ int main(void)
 	int n = 48;
 	
 	do {
+		if (n == 57)
+		{
+			putchar(n);
+			n++;
+		}
+		else
+		{
 		putchar(n);
 		putchar(44);
 		putchar(32);
 		n++;
+		}
 	} while(n < 58);
 
 	return (0);
