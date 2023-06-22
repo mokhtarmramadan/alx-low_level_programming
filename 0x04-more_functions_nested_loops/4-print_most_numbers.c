dc8329efd058e0f1109fb2ba6dadbada21ed9ba8
mokhatrmramadan <mokhtarramdanformal@gmail.com>
2023-06-22 10:43:10 -0700

    added print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9
+ * Parameters: void
+ * Returns: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i == 50 || i == 52)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(i);
+		}
+	}
+	_putchar(10);
+}
