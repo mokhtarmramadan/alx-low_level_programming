81abdd0ae83ffdfdbb95894d90fbd414fcc179f6
mokhatrmramadan <mokhtarramdanformal@gmail.com>
2023-06-22 10:36:46 -0700

    Added print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers
+ * Parameters: None
+ * Returns: Nothing.
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar(10);
+}
