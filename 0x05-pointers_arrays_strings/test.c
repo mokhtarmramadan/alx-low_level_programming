d249cfa1ce90122d799b441a42940306f9a15889
mokhatrmramadan <mokhtarramdanformal@gmail.com>
2023-06-26 08:00:33 -0700

    first commit

---
+++ 0x05-pointers_arrays_strings/test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void modif_my_param(int m)
+{
+   m = 402;
+}
+
+/**
+ * main - parameters are passed by value
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+   int n;
+
+   n = 98;
+   modif_my_param(n);
+   printf("%d\n", n);
+   return (0);
+}
