002c1e152ad6d75b5bae894f419e9f4d2dd4a5ff
mokhatrmramadan <mokhtarramdanformal@gmail.com>
2023-07-10 10:10:38 -0700

    added create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,6 +11,11 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 	char *buffer;
+	
+	if (size <= 0)
+	{
+		return("NULL");
+	}
 
 	buffer = malloc((size + 1) * sizeof(int));
 
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * strdup - function that returns a pointer to a newly allocated space
+ * @str: the the passed stirng
+ * Return: a pointer to the new string
+ */
+char *_strdup(char *str)
+{
+
