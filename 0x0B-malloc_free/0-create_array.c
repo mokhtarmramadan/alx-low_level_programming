b372c268a690c7ba9b80693815402a61df1cdb0f
mokhatrmramadan <mokhtarramdanformal@gmail.com>
2023-07-10 09:50:06 -0700

    Added all files

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array of chars
+ * @size: size of the array
+ * @c:charcter
+ * Return: array pointer
+ */
+char *create_array(unsigned int size, char c)
+{
+
