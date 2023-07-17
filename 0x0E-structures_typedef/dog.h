1703a9e67bf933a2ebaf32756f0fad28c791f2de
mokhatrmramadan <mokhtarramdanformal@gmail.com>
2023-07-17 14:47:16 -0700

    Added a new struct to dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,6 +16,21 @@ struct dog
 	char *owner;
 };
 
+/**
+ * struct dog_t - dog data type
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Dog's owner name
+ *
+ * Description: Dog datatype
+ */
+typedef struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
