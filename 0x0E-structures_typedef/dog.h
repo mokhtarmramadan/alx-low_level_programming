e1bec117d25668f128213319eb1b1eff838b9604
mokhatrmramadan <mokhtarramdanformal@gmail.com>
2023-07-17 10:14:56 -0700

    Added dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - dog data type
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Dog's owner name
+ *
+ * Description: Dog datatype
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* #ifndef _DOG_H_ */
