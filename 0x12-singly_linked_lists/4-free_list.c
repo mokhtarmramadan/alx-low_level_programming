2edcbab416430bac11e83ac6c62c43cbe1b3c9dc
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2023-07-27 06:19:41 -0700

    Added free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,13 +10,11 @@ void free_list(list_t *head)
 {
 	list_t *temp;
 
-	while (*head != NULL)
+	while (head != NULL)
 	{
 		temp = head;
 		head = head->next;
 		free(temp->str);
-		free(temp->len);
-		free(temp->next);
 		free(temp);
 	}
 }
