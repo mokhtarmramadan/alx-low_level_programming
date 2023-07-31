bc993ce057866e606b5f3f6187bd51b701a480dc
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2023-07-31 19:34:32 +0300

    style

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,19 +2,18 @@
 #define _LISTS_H_
 
 #include <stddef.h>
- 
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 
