55bd009684e5af9454a27bfbdf8a6363007bcc06
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2023-07-31 14:35:32 +0300

    final touch

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 
 /**
- * add_node_end - adds a new node at the end of a list_t list
+ * add_nodeint_end - adds a new node at the end of a list_t list
  * @head: the head of the list
- * @str: string to be added
+ * @n: int to be added
  * Return: the address of the new element, or NULL if it failed
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -2,19 +2,18 @@
 #include <stdlib.h>
 
 /**
- * free_list - that frees a list_t list
+ * free_listint - that frees a list_t list
  * @head: linked list head pointer
  * Returns: void
  */
 void free_listint(listint_t *head)
 {
-	list_t *temp;
+	listint_t *temp;
 
 	while (head != NULL)
 	{
 		temp = head;
 		head = head->next;
-		free(temp->str);
 		free(temp);
 	}
 }
