297c578e7cdab92958cca7a1ee1ab16ced5129b5
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2023-07-31 14:20:04 +0300

    added list len

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * list_len - a function that returns the number of elements in a linked
+ * listint_len - a function that returns the number of elements in a linked
  * @h: node pointer
  * Return: the number of nodes
  */
