bd5ce8623a6d90ad8798b78dce5ab4e7f6a8cbb8
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2023-10-06 16:42:16 +0300

    Added print hash

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,5 +1,5 @@
 #include "hash_tables.h"
-
+#include <stdbool.h>
 
 /**
  * hash_table_print - prints a hash table
@@ -10,22 +10,24 @@ void hash_table_print(const hash_table_t *ht)
 {
 	hash_table_t *table;
 	unsigned long int i;
-	int printed = 0;
+	bool flag = false;
 
 	table = (hash_table_t *)ht;
 	if (table == NULL || table->size == 0)
 		return;
+	printf("{");
 	for (i = 0; i < table->size; i++)
 	{
 		if (table->array[i])
 		{
+			if (flag)
+				printf(", ");
+
 			print_linked_list(table->array[i]);
-			printed++;
+			flag = true;
 		}
 	}
-	if (printed == 0)
-		printf("{}");
-	printf("\n");
+	printf("}\n");
 }
 
 
@@ -38,20 +40,12 @@ void print_linked_list(hash_node_t *head)
 {
 	hash_node_t *temp = head;
 
-	printf("{");
 	while (temp != NULL)
 	{
-		if (temp->next == NULL)
-		{
-			printf("'%s': '%s'", temp->key, temp->value);
-			temp = temp->next;
-		}
-		else
-		{
-			printf("'%s': '%s', ", temp->key, temp->value);
-			temp = temp->next;
-		}
+		printf("'%s': '%s'", temp->key, temp->value);
+		temp = temp->next;
+		if (temp != NULL)
+			printf(", ");
 	}
-	printf("}");
 
 }
