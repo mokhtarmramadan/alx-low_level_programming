9414a60f26d15e9e85676e80d7ae5ecd097fcc3e
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2023-10-06 17:26:21 +0300

    Added delete tables

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,38 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: a hash_table_t pionts at the table that will be deleted
+ * Returns: void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_table_t *table = ht;
+	unsigned long int i;
+
+	for (i = 0; i < table->size; i++)
+	{
+		if (table->array[i] != NULL)
+			free_list(table->array[i]);
+	}
+	free(table->array);
+	free(table);
+	ht = NULL;
+}
+
+
+void free_list(hash_node_t *list)
+{
+	hash_node_t *temp;
+	hash_node_t *head = list;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = temp->next;
+		free(temp->key);
+		free(temp->value);
+		free(temp);
+	}
+
+}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -46,5 +46,6 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
 void print_linked_list(hash_node_t *head);
+void free_list(hash_node_t *list);
 
 #endif /* #ifndef HASH_TABLES_H */
