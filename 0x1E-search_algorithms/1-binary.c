5bc9e21658679d8bb5bd03cea85c6c57b1536fb4
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2024-02-05 10:55:32 +0200

    02-05 10:55

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "search_algos.h"
+
+
+/**
+ * binary_search - searches for a value in a sorted array (Binary search)
+ * @array: a pointer to the array we want to search
+ * @size: size of array
+ * @value: the value we're searching for 
+ * Return: the index of the value
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t low, high, mid;
+
+	low = 0;
+	high = size - 1;
+	
+}
